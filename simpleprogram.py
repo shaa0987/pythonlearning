a7dc846e0b32c6e231fc370230eaab3424d0c34b
shaa0987 <110523091+shaa0987@users.noreply.github.com>
2025-06-08 00:32:11 +0530

    Add files via upload
    first changes...

---
+++ simpleprogram.py
@@ -0,0 +1,16 @@
+#print math operation on numbers
+n1 = int(input("number 1"))
+n2 = int(input("number 2"))
+
+print("Addition: ", n1 + n2)
+print("Subtraction: ", n1 - n2)
+print("Multiplication: ", n1 * n2)
+print("Division", n1/n2)
+
+#print greeting for the inout name.
+p1 = input("Enter the first name: ")
+p2 = input("Enter the last name: ")
+
+name = p1 + " " + p2
+
+print(f"Hello, {name}! Welcome to the Python program.")
